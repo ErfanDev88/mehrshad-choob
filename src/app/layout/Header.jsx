bc6c89f62296a3bfe1479c6fdb47516f8fce7df2
erfandev88 <erfangaaang@gmail.com>
2024-03-18 18:50:40 +0330

    add primary files and folders

---
+++ src/app/layout/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./_Header.module.scss"
+
+function Header() {
+  return (
+    <header className={styles.header}>
+cd
+    </header>
+  )
+}
+
+export default Header
---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Header from "./layout/Header";
+import styles from "./page.module.scss";
+
+export default function Home() {
+  return (
+    <main className={styles.container}>
+      <h1>مهرشاد</h1>
+    </main>
+  );
+}
