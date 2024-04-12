e81c15b2f01f385da846cb86691b65d3b1c5b679
erfandev88 <erfangaaang@gmail.com>
2024-04-12 21:58:17 +0330

    -a

--- src/app/components/AnimCursor.jsx
+++ src/app/components/AnimCursor.jsx
@@ -1,29 +1,32 @@
 import React from "react";
 import AnimatedCursor from "react-animated-cursor";
+import styles from './_Btn.module.scss'
 
 function AnimCursor() {
   return (
-    <AnimatedCursor
-      innerSize={10}
-      outerSize={31}
-      color="90, 75, 27"
-      outerAlpha={0.2}
-      innerScale={1}
-      outerScale={1.5}
-      clickables={[
-        "a",
-        'input[type="text"]',
-        'input[type="email"]',
-        'input[type="number"]',
-        'input[type="submit"]',
-        'input[type="image"]',
-        "label[for]",
-        "select",
-        "textarea",
-        "button",
-        ".link",
-      ]}
-    />
+    <div className={styles.animCursor}>
+      <AnimatedCursor
+        innerSize={10}
+        outerSize={31}
+        color="90, 75, 27"
+        outerAlpha={0.2}
+        innerScale={1}
+        outerScale={1.5}
+        clickables={[
+          "a",
+          'input[type="text"]',
+          'input[type="email"]',
+          'input[type="number"]',
+          'input[type="submit"]',
+          'input[type="image"]',
+          "label[for]",
+          "select",
+          "textarea",
+          "button",
+          ".link",
+        ]}
+      />
+    </div>
   );
 }
 
